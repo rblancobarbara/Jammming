8c927b8a6ae6cd115c9dd3e87998b23ce8842835
rblancobarbara <rblancobarbara@yahoo.com.ar>
2018-04-06 11:41:54 -0400

    Updating

--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -9,7 +9,7 @@ class Track extends React.Component {
       <div className="Track">
         <div className="Track-information">
           <h3>track name will go here</h3>
-          <p>track artist will go here | track album will go here </p>
+          <p>track artist will go here | track album will go here</p>
         </div>
         <a className="Track-action">"+ or - will go here"</a>
       </div>
