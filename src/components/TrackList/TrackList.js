c842a56b2da915a95c2189d81f35a000f15d2aa0
rblancobarbara <rblancobarbara@yahoo.com.ar>
2018-04-08 17:18:45 -0400

    Working on step 34

--- src/components/TrackList/TrackList.js
+++ src/components/TrackList/TrackList.js
@@ -8,9 +8,11 @@ class TrackList extends React.Component {
   render() {
     return (
       <div className="TrackList">
-        <Track track={this.props.track}/>
-        <Track />
-        <Track />
+      {this.props.tracks.map(track => {
+        return (
+          <Track />
+          )
+        })}
       </div>
     )
   }
