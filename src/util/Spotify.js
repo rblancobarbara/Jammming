29bf6cc77a709c647683a82a2b2d0ba8692b118c
rblancobarbara <rblancobarbara@yahoo.com.ar>
2018-04-19 11:08:25 -0400

    Step 83 Finished

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -1,4 +1,27 @@
+const clientId = "5c716839e03944d292029c2e4983c390";
+const redirectUri = "http://localhost:3000/";
 
+const userAccessToken;
+
+const Spotify = {
+  getAccessToken() {
+    if (userAccessToken) {
+      return userAccessToken;
+    } else {
+      const accessToken = window.location.href.match(/access_token=([^&]*)/);
+      const expiresIn = window.location.href.match(/expires_in=([^&]*)/);
+      window.setTimeout(() => accessToken = '', expiresIn * 1000);
+window.history.pushState('Access Token', null, '/');
+    } else {
+      window.location = `https://accounts.spotify.com/authorize?client_id=${client_id}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirect_uri}`;
+    }
+  }
+}
+
+export default Spotify;
+
+
+/*
 const xhr = new XMLHttpRequest();
 const url = "https://accounts.spotify.com/authorize";
 
@@ -12,14 +35,6 @@ xhr.onreadystatechange = function () {
 xhr.open("GET", url);
 xhr.send();
 
-
-const userAccessToken;
-
-getAccessToken() {
-  if (userAccessToken) {
-    return access_token = userAccessToken;
-  }
-}
 const urltoredirect = url + '?client_id=' + client_id + '&redirect_uri=' + redirect_uri + '&scope=' + scope + '&response_type=' + response_type + '&state=' + state
 "https://accounts.spotify.com/authorize?client_id=5fe01282e94241328a84e7c5cc169164&redirect_uri=http:%2F%2Fexample.com%2Fcallback&scope=user-read-private%20user-read-email&response_type=token&state=123"
 
@@ -40,10 +55,7 @@ state:
 
 let userAccessToken;
 
-let Spotify = {
-
-
-export default Spotify;
 
 window.setTimeout(() => accessToken = '', expiresIn * 1000);
 window.history.pushState('Access Token', null, '/');
+*/
